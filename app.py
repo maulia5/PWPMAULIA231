b0a6ae88dae6ff3223c9a525311d4dffd39259eb
Maulia Hafifatun Solihah <155612320+maulia5@users.noreply.github.com>
2024-12-31 07:31:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pengguna.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
